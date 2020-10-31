072893f2aefd07c00036af9d252a9d31e8b7ae75
Anshul1507 <ag1507anshul@gmail.com>
2020-10-31 17:12:31 +0530

    Added : Splash Screen

---
+++ app/src/main/java/tech/anshul1507/melodix/SplashActivity.kt
@@ -0,0 +1,11 @@
+package tech.anshul1507.melodix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
