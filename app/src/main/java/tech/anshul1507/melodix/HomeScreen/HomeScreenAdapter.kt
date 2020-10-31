63118a47191d1f49ee95c200620615c85fc4f7f7
Anshul1507 <ag1507anshul@gmail.com>
2020-11-01 00:33:40 +0530

    Added : Model Class & Adapter Structure

---
+++ app/src/main/java/tech/anshul1507/melodix/HomeScreen/HomeScreenAdapter.kt
@@ -0,0 +1,26 @@
+package tech.anshul1507.melodix.HomeScreen
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import tech.anshul1507.melodix.Songs
+
+class HomeScreenAdapter(songDetails: ArrayList<Songs>, context: Context): RecyclerView.Adapter<HomeScreenAdapter.MyViewHolder>(){
+
+    class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
