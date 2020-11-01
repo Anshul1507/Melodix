7a43a06afd306b60b304faec21ef70ea8285d161
Anshul1507 <ag1507anshul@gmail.com>
2020-11-01 19:36:17 +0530

    Added : Init Favorite Screen

--- app/src/main/java/tech/anshul1507/melodix/FavoriteScreen/FavoriteFragment.kt
+++ app/src/main/java/tech/anshul1507/melodix/FavoriteScreen/FavoriteFragment.kt
@@ -1,64 +1,52 @@
-package tech.anshul1507.melodix.HomeScreen
+package tech.anshul1507.melodix.FavoriteScreen
 
 import android.app.Activity
 import android.content.Context
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.provider.MediaStore
-import android.view.*
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.RelativeLayout
 import android.widget.TextView
-import android.widget.Toolbar
-import androidx.recyclerview.widget.DefaultItemAnimator
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import tech.anshul1507.melodix.R
 import tech.anshul1507.melodix.Models.Songs
+import tech.anshul1507.melodix.R
 
+class FavoriteFragment : Fragment() {
 
-class HomeFragment : Fragment() {
-
-    private var songsList: ArrayList<Songs>? = null
+    private var myActivity: Activity? = null
+    private var noFavorites: TextView? = null
     private var nowPlayingBottomBar: RelativeLayout? = null
     private var playPauseButton: ImageButton? = null
     private var songTitle: TextView? = null
-    private var contentMainLayout: RelativeLayout? = null
-    private var noSongsLayout: RelativeLayout? = null
-    private var mainRecyclerView: RecyclerView? = null
-    private var myActivity: Activity? = null
-    private var homeScreenAdapter: HomeScreenAdapter? = null
+    private var favRecyclerView: RecyclerView? = null
     private var trackPosition: Int = 0
-    private var trackTitle: TextView? = null
+    private var favSongsList: ArrayList<Songs>? = null
 
-    object Statified {
+    object FavObject {
         var mediaPlayer: MediaPlayer? = null
-        var flag: Int? = 0
-        var toolbar: Toolbar? = null
+        var flag: Int = 0
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        activity?.title = "All Songs"
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-        initViews(view)
+        myActivity?.title = "Favorites"
         setHasOptionsMenu(true)
-        Statified.toolbar = view?.findViewById(R.id.toolbar)
-        return view
-    }
-
-    private fun initViews(view: View) {
+        val view = inflater.inflate(R.layout.fragment_favorite, container, false)
+        noFavorites = view?.findViewById(R.id.no_favorite_layout)
         nowPlayingBottomBar = view.findViewById(R.id.bottom_play_layout)
-        playPauseButton = view.findViewById(R.id.btn_play_pause_fav)
         songTitle = view.findViewById(R.id.song_title_text_fav)
-        contentMainLayout = view.findViewById(R.id.content_main_layout)
-        noSongsLayout = view.findViewById(R.id.no_songs_layout)
-        mainRecyclerView = view.findViewById(R.id.main_recycler_view)
-        trackTitle = view.findViewById(R.id.track_title)
+        playPauseButton = view.findViewById(R.id.btn_play_pause_fav)
+        favRecyclerView = view.findViewById(R.id.favorite_recycler_view)
 
+        return view
     }
 
     override fun onAttach(context: Context) {
@@ -71,47 +59,14 @@ class HomeFragment : Fragment() {
         myActivity = activity
     }
 
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        val item = menu.findItem(R.id.action_sort)
+        item?.isVisible = false
+    }
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //TODO:: local db methods
 
-        bottomBarSetup()
-
-        songsList = getSongsFromDevice()
-        if (songsList.isNullOrEmpty()) {
-            contentMainLayout?.visibility = View.INVISIBLE
-            noSongsLayout?.visibility = View.VISIBLE
-        } else {
-            //TODO:: set adapter for main recycler view
-            homeScreenAdapter =
-                HomeScreenAdapter(songsList as ArrayList<Songs>, myActivity as Context)
-            mainRecyclerView?.layoutManager = LinearLayoutManager(myActivity)
-            mainRecyclerView?.itemAnimator = DefaultItemAnimator()
-            mainRecyclerView?.adapter = homeScreenAdapter
-        }
-
-        //TODO:: sort songs according to prefs
-
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        menu.clear()
-        inflater.inflate(R.menu.main_menu, menu)
-        return
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.action_sort_ascending -> {
-                if (!songsList.isNullOrEmpty()) {
-                    //TODO:: Save to local db and sort accordingly
-                }
-            }
-            R.id.action_sort_recent -> {
-                //TODO:: Save to local db and sort accordingly
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     fun bottomBarSetup() {
@@ -138,7 +93,7 @@ class HomeFragment : Fragment() {
      * Function to fetch Songs from Device
      */
     private fun getSongsFromDevice(): ArrayList<Songs> {
-        var list = ArrayList<Songs>()
+        val list = ArrayList<Songs>()
         val contentResolver = myActivity?.contentResolver
         val songUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
         val songCursor = contentResolver?.query(songUri, null, null, null, null)
